7d361933835cbebf1e6d75334642a86a66d1a5e7
Blake Nicholson <blaken2021@gmail.com>
2016-09-25 15:00:14 -0400

    added images and other js files

--- sticky-navbar/js/main.js
+++ sticky-navbar/js/main.js
@@ -6,3 +6,15 @@ $(".animsition").animsition({
   outDuration: 500
 });
 //$("")nth-child(3).addClass("sticky button");
+$('.header').sticky();
+
+$('.header').on('sticky-start', function(){
+    $('.description').html('We build <strong>great</strong>apps');
+
+});
+$('.header').on('sticky-end', function(){
+    $('.description').html('We build apps');
+
+});
+
+//$('.work').sticky({})
